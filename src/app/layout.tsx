71b4b9af08085632fe4cf790e4a1ac4d10adbf3f
pdrhp <pedrohsp2004@gmail.com>
2025-01-07 00:39:20 -0300

    feat: enhance layout with ThemeProvider, SignalRProvider, and Toaster integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { SignalRProvider } from "@/context/SignalRContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+          storageKey="matchmovie-theme"
+        >
+          <SignalRProvider>{children}</SignalRProvider>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
